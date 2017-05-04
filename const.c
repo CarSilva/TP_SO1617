6d86941776bec440e5f370ffff36ad6c33d26b95
Francisco <a75097@alunos.uminho.pt>
2017-05-04 23:17:38 +0100

    debugging const

--- const.c
+++ const.c
@@ -9,7 +9,7 @@ ssize_t readln(int fildes, char *buf, size_t nbyte){
 	while((i<nbyte) && (rd=read(fildes,buf+i,1)) > 0 && *(buf+i) != '\n'){
 		i++;
 	}
-	return ++i;
+	return i;
 }
 
 
@@ -17,23 +17,25 @@ int main (int argc, char** argv){
 	char *buf = (char *) malloc(1024);
 	char *aux = (char *) malloc(strlen(buf) + strlen(argv[1])+1);
 	
-	aux[0]='\0';
+	//aux[0]='\0';
 	
 	int n = 1,a;
 	int sn;
 	
 
-	while((a = readln(0,buf,1024))>1){
+	while((a = readln(0,buf,1024))>0){
 		aux = (char *) realloc(aux, strlen(buf) + strlen(argv[1])+1);
 		strcat(aux,buf);
+		aux[a]='\0';
 		strcat(aux,":");
 		strcat(aux,argv[1]);
 		printf("%s",aux);
 		
 	}
+
 	free(aux);
 	free(buf);
-
+	
 	return 0;
 	
 }
