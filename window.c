23490f57ae2ec80ae95ec0b85cfaf5285edb64d5
Carlos <carlosmms96@gmail.com>
2017-05-07 00:01:24 +0100

    Window só a faltar verificar letras e um \n que nao sei porque aparece

--- window.c
+++ window.c
@@ -19,39 +19,43 @@ void put(char *buf, int n){
 	snprintf(aux, len+2, ":%d", n);
 	aux2[len-1] = '\0';
 	strcat(aux2, aux);
-	printf("(%s)\n",aux2 );
+	printf("%s\n",aux2 );
 }
 
 
 int main(int argc, char const *argv[]) {
 	char *tmp, *tok, *buf = malloc(1024);
 	int *ops = malloc(sizeof(int)*1024);
-	int n, i,k = 0, nL = 0;
+	int n, i, k = 0, nL = 0, j = 0, tp = 0;
 	while((n = readln(0, buf, 1024)) > 1){
 		tmp = strdup(buf);
 		tok = strtok(tmp, ":");
 		for(i = 1; tok; i++){
-			if(i ==  atoi(argv[1])){
-				if(k >= atoi(argv[3]))
-					k = 0;
+			if(i ==  atoi(argv[1]))
 				ops[k] = atoi(tok);
-			}
 			tok = strtok(NULL, ":");
 		}
-		if(nL < atoi(argv[3])){
+		if(nL == 0){
 			put(buf, 0);
 		}else{
 			if(!strcmp("avg", argv[2])){
-				int media, sum = 0;
-				for(i = 0; i < atoi(argv[3]); i++){
+				int media, sum = 0, nargs = atoi(argv[3]);
+				for(i = nL - nargs; i < nL; i++){
+					while(i < 0 ){
+						i++;
+						nargs--;
+					}
 					sum += ops[i];
 				}
-				media = sum/atoi(argv[3]);
+				media = sum/nargs;
 				put(buf, media);
 			}
 			if(!strcmp("min", argv[2])){
 				int curr, min;
-				for(i = 0; i < atoi(argv[3]); i++){
+				for(i = nL - atoi(argv[3]); i < nL; i++){
+					while(i < 0 ){
+						i++;
+					}
 					curr = ops[i];
 					if(i == 0)
 						min = curr;
@@ -62,7 +66,10 @@ int main(int argc, char const *argv[]) {
 			}
 			if(!strcmp("max", argv[2])){
 				int curr, max = -1;
-				for(i = 0; i < atoi(argv[3]); i++){
+				for(i = nL - atoi(argv[3]); i < nL; i++){
+					while(i < 0 ){
+						i++;
+					}
 					curr = ops[i];
 					if(curr > max)
 						max = curr;
@@ -71,7 +78,10 @@ int main(int argc, char const *argv[]) {
 			}
 			if(!strcmp("sum", argv[2])){
 				int sum = 0;
-				for(i = 0; i < atoi(argv[3]); i++){
+				for(i = nL - atoi(argv[3]); i < nL; i++){
+					while(i < 0 ){
+						i++;
+					}
 					sum += ops[i];
 				}
 				put(buf, sum);
